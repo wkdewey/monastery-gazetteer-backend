7d46aeb147b27a961733f1134f69d2a6463f2535
wkdewey <wkdewey@gmail.com>
2020-09-10 16:07:05 -0400

    add routes and show and index actions for monasteries

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/test', to: 'application#test'
+  namespace :api do
+    namespace :v1 do
+      resources :monasteries, only: [:index, :show]
+    end
+  end
 end
