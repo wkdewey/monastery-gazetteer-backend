7857a1a5765eda0ae1bcaa7c6cc6a48477483c5e
wkdewey <wkdewey@gmail.com>
2020-09-10 15:22:19 -0400

    run migrations and seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+monasteries = Monastery.create([
+  { name: 'Sera Me', location: 'Lhasa, Tibet', religious_tradition: 'Geluk' },
+  { name: 'Alchi', location: 'Ladakh', religious_tradition: 'Geluk' },
+  { name: 'Chagri', location: 'Bhutan', religious_tradition: 'Kagyu' },
+  { name: 'Dorje Drak', location: 'Lhoka, Tibet', religious_tradition: 'Nyingma' },
+  { name: 'Drepung', location: 'Lhasa, Tibet', religious_tradition: 'Geluk' },
+  { name: 'Drigung', location: 'Lhasa, Tibet', religious_tradition: 'Kagyu' },
+  { name: 'Dzogchen', location: 'Kham, Tibet', religious_tradition: 'Nyingma' },
+  { name: 'Ganden', location: 'Lhasa, Tibet', religious_tradition: 'Geluk' },
+  { name: 'Hemis', location: 'Ladakh', religious_tradition: 'Drukpa Kagyu '},
+  { name: 'Jonang', location: 'Tsang, Tibet', religious_tradition: 'Jonang'}
+])
