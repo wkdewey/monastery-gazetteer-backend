a2b5f93253aae1f85934b6d50ace3718530a99f0
wkdewey <wkdewey@gmail.com>
2020-09-11 19:53:53 -0400

    run migrations for new tables

--- db/migrate/20200911234110_create_monastery_figures.rb
+++ db/migrate/20200911234110_create_monastery_figures.rb
@@ -1,8 +1,8 @@
 class CreateMonasteryFigures < ActiveRecord::Migration[6.0]
   def change
     create_table :monastery_figures do |t|
-      t.references :Monastery, null: false, foreign_key: true
-      t.references :Figures, null: false, foreign_key: true
+      t.references :monastery, null: false, foreign_key: true
+      t.references :figures, null: false, foreign_key: true
 
       t.timestamps
     end
