caed02b82cfc1eb8817b9f7941b9af45a44efed2
wkdewey <wkdewey@gmail.com>
2020-09-13 17:27:16 -0400

    add figures route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :monasteries, only: [:index, :show]
+      resources :figures, only: [:index, :show]
     end
   end
 end
