f9f085d1915cda47a3a9a1b6ef28e03f5d5cb77d
wkdewey <wkdewey@gmail.com>
2020-09-14 12:36:30 -0400

    add join model attributes to serializers

--- app/serializers/monastery_serializer.rb
+++ app/serializers/monastery_serializer.rb
@@ -1,4 +1,4 @@
 class MonasterySerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :location, :religious_tradition
+  attributes :name, :location, :religious_tradition, :figures
 end
