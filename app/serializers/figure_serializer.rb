d7543d2e9140e66bf0ba6bac17101ff12c6b4325
wkdewey <wkdewey@gmail.com>
2020-09-16 09:22:38 -0400

    adjust params and serializer for monasteries belonging to figure

--- app/serializers/figure_serializer.rb
+++ app/serializers/figure_serializer.rb
@@ -1,4 +1,4 @@
 class FigureSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :lifespan, :religious_tradition
+  attributes :name, :lifespan, :religious_tradition, :monasteries
 end
