04b99a85565fe7f276bdbeb3af2f01deef023269
wkdewey <wkdewey@gmail.com>
2020-09-16 14:27:06 -0400

    fix accepts nested attributes again and add end

--- app/models/monastery.rb
+++ app/models/monastery.rb
@@ -2,4 +2,5 @@ class Monastery < ApplicationRecord
   has_many :monastery_figures
   has_many :figures, through: :monastery_figures
   
-  accepts_nested_attributes_for: :monastery_figures
+  accepts_nested_attributes_for :monastery_figures
+end
