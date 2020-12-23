6b2e168645c821bdea216fbd998b6db87bfdc17b
wkdewey <wkdewey@gmail.com>
2020-12-22 19:44:28 -0600

    add attached image to models

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,6 +1,7 @@
 class Figure < ApplicationRecord
   has_many :monastery_figures
   has_many :monasteries, through: :monastery_figures, dependent: :destroy
+  has_one_attached :image
 
   accepts_nested_attributes_for :monasteries
 end
--- app/models/monastery.rb
+++ app/models/monastery.rb
@@ -1,6 +1,7 @@
 class Monastery < ApplicationRecord
   has_many :monastery_figures
   has_many :figures, through: :monastery_figures, dependent: :destroy
+  has_one_attached :image
   
   accepts_nested_attributes_for :figures
 end
