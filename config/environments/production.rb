4a31710dd5a4642ad263d0534bc0335d0a522229
wkdewey <wkdewey@gmail.com>
2021-02-01 20:16:51 -0500

    undo last commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  deliberatelybreakingmycode
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
@@ -104,5 +103,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   #change the below when I actually deploy the application
-  Rails.application.routes.default_url_options = { host: "http://www.example.com"}
+  Rails.application.routes.default_url_options = { host: "http://www.example.com" }
 end
