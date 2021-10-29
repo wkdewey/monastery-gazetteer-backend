2633d941507f09e0a9b03b06343d98a42deb1b7b
wkdewey <william.dewey@unl.edu>
2021-10-29 13:23:06 -0500

    make sure the figures on join model are optional

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -4,7 +4,7 @@ class Figure < ApplicationRecord
   has_many :monastery_figures
   has_many :monasteries, through: :monastery_figures, dependent: :destroy
   has_one_attached :image
-  belongs_to :monastery_figures
+  belongs_to :monastery_figure, optional: true
 
   accepts_nested_attributes_for :monasteries
 
--- db/migrate/20211015185431_add_monastery_figures_to_figures.rb
+++ db/migrate/20211015185431_add_monastery_figures_to_figures.rb
@@ -1,5 +1,5 @@
 class AddMonasteryFiguresToFigures < ActiveRecord::Migration[6.0]
   def change
-    add_reference :figures, :monastery_figures, null: :true
+    add_reference :figures, :monastery_figure, index: false, null: :true
   end
 end
