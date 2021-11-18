c0112abc4828d49db0f009b2ce020520a98f84f8
wkdewey <william.dewey@unl.edu>
2021-11-17 20:12:53 -0600

    clarify model relationships

--- app/models/monastery.rb
+++ app/models/monastery.rb
@@ -2,7 +2,7 @@ class Monastery < ApplicationRecord
   include Rails.application.routes.url_helpers
 
   has_many :monastery_figures
-  has_many :figures, through: :monastery_figures, dependent: :destroy
+  has_many :figures, {:through => :monastery_figures, :dependent => :destroy, :source => :monastery}
   has_one_attached :image
   
   accepts_nested_attributes_for :figures
