b62b2a95c64a39d4d31ba31b0e0cf7b38e6808d1
wkdewey <william.dewey@unl.edu>
2021-12-03 11:49:49 -0600

    add monastery_figures to attributes

--- app/serializers/figure_serializer.rb
+++ app/serializers/figure_serializer.rb
@@ -1,6 +1,6 @@
 class FigureSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :birth_date, :death_date, :biography, :religious_tradition, :monasteries
+  attributes :name, :birth_date, :death_date, :biography, :religious_tradition, :monasteries, :monastery_figures
 
   attribute :image_url do |figure|
     figure.get_image_url()
